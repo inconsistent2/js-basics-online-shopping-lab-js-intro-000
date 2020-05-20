0a699b2358eda78200f245a95f2fa747f65ef4da
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-20 23:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,8 +64,10 @@ function placeOrder(cardNumber) {
     return 'Sorry, we don\'t have a credit card on file for you.'
   } else {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
-    var cart=[]
+    for(var i=0; i<getCart().length; i++){
+      getCart().pop()
   }
+}
 }
 
 function generateItem(itemName){
